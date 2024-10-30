ab4d2ade390bcccbe910fa01351914230c50c36c
SKD <seankyled1st@gmail.com>
2024-10-30 16:35:33 +0800

    8th Commit

---
+++ src/main/java/ccinfom/group5/esports_app/model/tables/Player.java
@@ -0,0 +1,8 @@
+package ccinfom.group5.esports_app.model.tables;
+
+public class Player {
+    
+    public Player() {
+        
+    }
+}
---
+++ src/main/java/ccinfom/group5/esports_app/model/tables/Team.java
@@ -0,0 +1,8 @@
+package ccinfom.group5.esports_app.model.tables;
+
+public class Team {
+    
+    public Team() {
+        
+    }
+}

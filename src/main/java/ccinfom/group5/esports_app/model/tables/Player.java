295b40571dc8160533f819d5ff6e31cfc02d3790
Megan Kei Dasal <157576956+megandasal@users.noreply.github.com>
2024-11-18 13:20:55 +0800

    Update Player.java

--- src/main/java/ccinfom/group5/esports_app/model/tables/Player.java
+++ src/main/java/ccinfom/group5/esports_app/model/tables/Player.java
@@ -2,51 +2,52 @@ package ccinfom.group5.esports_app.model.tables;
 
 public class Player {
 
-    private int playerID;
-    private String lastName;
-    private String firstName;
+    private int player_id;
+    private String last_name;
+    private String first_name;
     private int age;
     private String country;
-    private String teamName;
+    private String current_team;
+    private String status;
 
-
-    public Player(int playerID, String lastName, String firstName, int age, String country, String teamName) {
-        this.playerID = playerID;
-        this.lastName = lastName;
-        this.firstName = firstName;
+    public Player(int player_id, String last_name, String first_name, int age, String country, String current_team, String status) {
+        this.player_id = player_id;
+        this.last_name = last_name;
+        this.first_name = first_name;
         this.age = age;
         this.country = country;
-        this.teamName = teamName;
+        this.current_team = current_team;
+        this.status = status;
     }
 
     public String getAllDetails() {
         String details;
 
-        details = playerID+", "+"\'"+lastName+"\'"+", "+"\'"+firstName+"\'"+", "+age+", "+
-                  "\'"+country+"\'"+", "+"\'"+teamName+"\'";
+        details = player_id+", "+"\'"+last_name+"\'"+", "+"\'"+first_name+"\'"+", "+age+", "+
+                  "\'"+country+"\'"+", "+"\'"+current_team+"\'"+", "+"\'"+status+"\'";
 
         return details;
     }
 
     public int getPlayerID() {
-        return playerID;
+        return player_id;
     }
-    public void setPlayerID(int playerID) {
-        this.playerID = playerID;
+    public void setPlayerID(int player_id) {
+        this.player_id = player_id;
     }
 
     public String getLastName() {
-        return lastName;
+        return last_name;
     }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public void setLastName(String last_name) {
+        this.last_name = last_name;
     }
 
     public String getFirstName() {
-        return firstName;
+        return first_name;
     }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void setFirstName(String first_name) {
+        this.first_name = first_name;
     }
 
     public int getAge() {
@@ -63,11 +64,19 @@ public class Player {
         this.country = country;
     }
 
-    public String getTeamName() {
-        return teamName;
+    public String getCurrentTeam() {
+        return current_team;
     }
-    public void setTeamName(String teamName) {
-        this.teamName = teamName;
+    public void setCurrentTeam(String current_team) {
+        this.current_team = current_team;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 
 }

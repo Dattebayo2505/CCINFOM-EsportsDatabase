272ad15ad496a494f54968824e5f545175d700f1
SKD <seankyled1st@gmail.com>
2024-11-19 18:08:11 +0800

    refactor: Put csv and sql files in "src/main/resources/"

--- src/main/resources/sql/TeamStats.sql
+++ src/main/resources/sql/TeamStats.sql
@@ -1,4 +1,4 @@
-INSERT INTO teamstats (team_name, total_winnings, favored_map, wins, losses) VALUES
+INSERT INTO teamstats (team, total_winnings, favored_map, wins, losses) VALUES
 ("DRX", 332000, "Bind", 11, 4),
 ("EDWard Gaming", 186000, "Split", 7, 7),
 ("FUT Esports", 140000, "Lotus", 6, 9),

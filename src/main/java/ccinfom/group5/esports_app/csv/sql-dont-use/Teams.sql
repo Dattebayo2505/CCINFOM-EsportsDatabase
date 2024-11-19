45762774bccb531aadfb9d13e8bc108a8c580dbe
Keisha Cu <keisha.cu050@gmail.com>
2024-11-19 12:39:07 +0800

    Converted tables (csv) to sql format

---
+++ src/main/java/ccinfom/group5/esports_app/csv/sql-dont-use/Teams.sql
@@ -0,0 +1,11 @@
+INSERT INTO teams (team_name, captain, country, region, status) VALUES
+('DRX', 'MaKo', 'South Korea', 'Pacific', 'active'),
+('EDward Gaming', 'CHICHOO', 'China', 'China', 'active'),
+('FUT Esports', 'ATA KAPTAN', 'Turkey', 'Europe Middle East and Africa', 'active'),
+('Fnatic', 'Boaster', 'United Kingdom', 'Europe Middle East and Africa', 'active'),
+('G2 Esports', 'valyne', 'United States', 'Americas', 'active'),
+('Paper Rex', 'd4v41', 'Singapore', 'Pacific', 'active'),
+('Sentinels', 'Sacy', 'United Stated', 'Americas', 'active'),
+('Cloud9', 'xeta', 'United States', 'Americas', 'active'),
+('Dattebayo', 'nTony', 'Philippines', 'Pacific', 'active'),
+('CCS Nation', 'lakambiini', 'France', 'Europe Middle East and Africa', 'active');
--- src/main/java/ccinfom/group5/esports_app/csv/sql-dont-use/esports.sql
+++ src/main/java/ccinfom/group5/esports_app/csv/sql-dont-use/esports.sql
@@ -1,27 +1,81 @@
 -- SERVES AS AN ALTERNATIVE OR BASIS
 
+-- Drop the table if it exists
+DROP TABLE IF EXISTS players;
+
 -- Create the database
 CREATE DATABASE IF NOT EXISTS esports;
 
 -- Use the database
 USE esports;
 
--- Drop the table if it exists
-DROP TABLE IF EXISTS players;
-
 -- Create the table
-CREATE TABLE players (
+CREATE TABLE companies (
     id INT PRIMARY KEY,
-    name VARCHAR(50),
-    age INT
+    company_name VARCHAR(50)
 );
 
--- Insert rows into the table
-INSERT INTO players (id, name, age) VALUES (1, 'Alice', 30);
-INSERT INTO players (id, name, age) VALUES (2, 'Guo', 25);
-INSERT INTO players (id, name, age) VALUES (3, 'Job', 35);
-INSERT INTO players (id, name, age) VALUES (4, 'SK', 28);
-INSERT INTO players (id, name, age) VALUES (5, 'Meg', 22);
+CREATE TABLE teams(
+    team INT PRIMARY KEY,
+    captain VARCHAR(50),
+    country VARCHAR(50),
+    region  VARCHAR(50),
+    status VARCHAR(50)
+);
 
+CREATE TABLE players (
+    player_id INT PRIMARY KEY, 
+    last_name VARCHAR(50), first_name VARCHAR(50),
+    age INTEGER, country VARCHAR(50), 
+    FOREIGN KEY (current_team) REFERENCES teams(team),
+    status VARCHAR(10)
+);
 
+CREATE TABLE playerhistory (
+    history_id INT PRIMARY KEY,
+    FOREIGN KEY(player_id) REFERENCES players(player_id),
+    FOREIGN KEY(old_team) REFERENCES teams(team),
+    left_old_team DATE,
+    FOREIGN KEY(new_team) REFERENCES teams(team),
+    joined_new_team DATE
+);
 
+CREATE TABLE sponsorhistory(
+    history_id INT PRIMARY KEY,
+    sponsor_id INT PRIMARY KEY,
+    FOREIGN KEY (team_name) REFERENCES teams(team),
+    contract_amount FLOAT(6, 2),
+    contract_start DATE,
+    contract_end DATE
+);
+
+CREATE TABLE teamhistory(
+    history_id INT PRIMARY,
+    FOREIGN KEY(team_id) REFERENCES teams(team),
+    creation_date DATE,
+    disband_date DATE
+);
+
+CREATE TABLE teamperformancehistory(
+    history_id INT PRIMARY KEY,
+    FOREIGN KEY(team_name) REFERENCES teams(team),
+    match_date DATE,
+    result VARCHAR (10),
+    winnings FLOAT(6, 2)
+);
+
+CREATE TABLE teamsponsor(
+    sponsor_id INT PRIMARY KEY, 
+    FOREIGN KEY(team) REFERENCES teams(team),
+    contract_amount FLOAT(6, 2), 
+    contract_start DATE, 
+    contract_end DATE
+);
+
+CREATE TABLE teamstats(
+    FOREIGN KEY(team) REFERENCES teams(team),
+    total_winnings FLOAT(7, 2),
+    favored_map VARCHAR (20),
+    wins INT,
+    losses INT
+);

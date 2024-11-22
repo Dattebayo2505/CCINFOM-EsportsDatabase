17eceb2610e82cebdafa70041d165c7edd1b29b0
SKD <seankyled1st@gmail.com>
2024-11-23 00:45:53 +0800

    add: Transactions class added to repo

--- src/main/java/ccinfom/group5/esports_app/model/Transaction.java
+++ src/main/java/ccinfom/group5/esports_app/model/Transaction.java
@@ -3,23 +3,31 @@ package ccinfom.group5.esports_app.model;
 import ccinfom.group5.esports_app.model.tables.*;
 
 public class Transaction {
+    private Database database;
 
-    Transaction() {
+    Transaction(Database database) {
+        this.database = database;
     }
 
 
-    public static void playerTransfer(Database database, 
-                                Player player, 
-                                String leftOldTeam, 
-                                String newTeam,
-                                String joinedNewTeam) {
+    public void playerTransfer(Player player, 
+                               String leftOldTeam, 
+                               String newTeam,
+                               String joinedNewTeam) {
 
         int lastHistory = database.getAllPlayerHistories().size();
-        database.getAllPlayerHistories().add(new PlayerHistory(lastHistory + 1, player.getPlayerID(), 
-                                                                player.getCurrentTeam(), leftOldTeam, 
-                                                                newTeam, joinedNewTeam));
+        database.getAllPlayerHistories().add(new PlayerHistory(lastHistory + 1, 
+                                                player.getPlayerID(), 
+                                                player.getCurrentTeam(), 
+                                                leftOldTeam, 
+                                                newTeam, 
+                                                joinedNewTeam));
 
         player.setCurrentTeam(newTeam);
         player.setStatus("active");
     }
+
+
+
+
 }

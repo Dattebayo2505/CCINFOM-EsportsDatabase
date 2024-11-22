71416fe1b2970a1c401c08c1e245c5df0ec5e36c
lozadajobd <68721231+lozadajobd@users.noreply.github.com>
2024-11-23 01:56:56 +0800

    Added a setTeamHistory

--- src/main/java/ccinfom/group5/esports_app/model/Transaction.java
+++ src/main/java/ccinfom/group5/esports_app/model/Transaction.java
@@ -27,7 +27,36 @@ public class Transaction {
         player.setStatus("active");
     }
 
+    public void setTeamHistory(Team team, String disbandDate) {
+        // Disband the team by updating team history
+        int lastHistory = database.getAllTeamHistories().size();
 
+        TeamHistory teamHistory = new TeamHistory(lastHistory + 1, 
+                                                  team.getTeamName(), 
+                                                  team.getTeamHistory(database), 
+                                                  disbandDate);
+        database.getAllTeamHistories().add(teamHistory);
 
+        // Set all players in the team to "inactive" and update their histories
+        for (Player player : database.getAllPlayers()) {
+            if (player.getCurrentTeam().equals(team.getTeamName())) {
+                String playerID = player.getPlayerID();
+                String leftOldTeamDate = disbandDate;
 
+                // Update player history
+                int playerLastHistory = database.getAllPlayerHistories().size();
+                PlayerHistory playerHistory = new PlayerHistory(playerLastHistory + 1, 
+                                                                playerID, 
+                                                                player.getCurrentTeam(), 
+                                                                leftOldTeamDate, 
+                                                                null, 
+                                                                null);
+                database.getAllPlayerHistories().add(playerHistory);
+
+                // Set player status to inactive
+                player.setCurrentTeam(null);
+                player.setStatus("inactive");
+            }
+        }
+    }
 }

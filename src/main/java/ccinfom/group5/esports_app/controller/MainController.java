95fa6ae76eabc2a8d3746818c77c1d0532ab9c23
SKD <seankyled1st@gmail.com>
2024-11-22 22:09:45 +0800

    add: Fix Table-view in GUI

--- src/main/java/ccinfom/group5/esports_app/controller/MainController.java
+++ src/main/java/ccinfom/group5/esports_app/controller/MainController.java
@@ -10,10 +10,12 @@ import javax.swing.table.TableColumnModel;
 
 import ccinfom.group5.esports_app.model.*;
 import ccinfom.group5.esports_app.view.GUI;
+import ccinfom.group5.esports_app.view_deprecate.OldGUI;
 
 public class MainController implements ActionListener {
     
     private Database database;
+    private OldGUI oldGUI;
     private GUI gui;
 
     public MainController(Database database, GUI gui) {
@@ -23,11 +25,19 @@ public class MainController implements ActionListener {
         initializeTable();
     }
 
+    public MainController(Database database, OldGUI oldGUI) {
+        this.database = database;
+        this.oldGUI = oldGUI;     
+        
+        initializeTable();
+    }
+
     private void initializeTable() {
         // TODO: Implement TableColumnModel for companies table testing
         String[] columnNames = database.getTableColumnNameMap().get("companies");
         Object[][] data = database.getTableDataMap().get("companies");
 
+        System.out.println("Number of rows: " + data.length);
         DefaultTableModel model = new DefaultTableModel(data, columnNames) {
             @Override
             public boolean isCellEditable(int row, int column) {
@@ -41,6 +51,8 @@ public class MainController implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
+
+        
     }
 
 }
